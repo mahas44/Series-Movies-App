400bdbc31ed7ac92e9f022a10069992bd1bc2747
Enes Mahmut ÇAKMAK <enescakmak44@gmail.com>
2020-03-01 15:10:12 +0300

    Update BaseConstant.java

--- app/src/main/java/com/enes/moviesapp/BaseConstant.java
+++ app/src/main/java/com/enes/moviesapp/BaseConstant.java
@@ -3,7 +3,8 @@ package com.enes.moviesapp;
 public interface BaseConstant {
 
 
-    String YouTube_API_KEY = "AIzaSyCHsd3vZ5vPK3lMstO2lg-nMRHBOJ_SBSg";
-    String SHA_1_FINGER_PRINT = "C7:45:E7:EA:90:A8:67:4F:F3:0E:69:DC:0E:90:5E:9A:B1:FA:BD:F4";
-    String TMDB_API_KEY = "fd50bae5852bf6c2e149317a6e885416";
+    String YouTube_API_KEY = "";
+    String SHA_1_FINGER_PRINT = "";
+    
+    String TMDB_API_KEY = "";
 }
